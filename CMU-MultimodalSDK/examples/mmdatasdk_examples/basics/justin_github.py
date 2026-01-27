64b916ad5730b119a583423c3d67586a78416143
hivehubapk-alt <hivehubapk@gmail.com>
2026-01-27 20:32:36 +0900

    Delete CMU-MultimodalSDK directory

--- CMU-MultimodalSDK/examples/mmdatasdk_examples/basics/justin_github.py
+++
@@ -1,2 +0,0 @@
-print ("Some of the content in this tutorial may be outdated, however it is an amazing tutorial nonetheless: https://github.com/Justin1904/CMU-MultimodalSDK-Tutorials")
-print ("Special thanks to Zhun Liu @ justin1904")
